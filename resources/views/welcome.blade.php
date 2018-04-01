f94cd456b7aaca802d95c575c5891cd3e37ae557
Amitav Roy <reachme@amitavroy.com>
2018-04-01 09:33:00 +0530

    Work in progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel WIP
+                    Laravel WIP 2
                 </div>
             </div>
         </div>
